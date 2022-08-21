50d749a3392974ae05f4d5a882f7a2287cfcf96d
FranSuarez99 <fransua@javerianacali.edu.co>
2022-08-21 18:31:32 -0500

    put

--- main.py
+++ main.py
@@ -26,3 +26,11 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int, item: Item):
+    return Item.text
+
+@app.post("/items/{item_id}")
+def post_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
