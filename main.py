b4aef92f2d1294b13be7dfe27f8d0a46307b1d91
FranSuarez99 <fransua@javerianacali.edu.co>
2022-08-21 17:34:07 -0500

    app

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel # adicionar en los imports en el main.py
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+app = FastAPI()
+
+
+app.get("/")
+def read_root():
+    url = 'https://630287099eb72a839d7105f1.mockapi.io/items'
+    response = requests.get(url, {}, timeout=5)
+    return {"items": response.json() }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
