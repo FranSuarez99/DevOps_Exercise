85a7298ddef8860c3c129d77c2cd50b503f70ca2
FranSuarez99 <fransua@javerianacali.edu.co>
2022-08-24 17:36:34 -0500

    logsPrometheus

--- main.py
+++ main.py
@@ -1,8 +1,67 @@
 import requests
+import logging
+from logging.config import dictConfig
 from typing import Union
-
+from prometheus_client import Counter, start_http_server
 from fastapi import FastAPI
 from pydantic import BaseModel # adicionar en los imports en el main.py
+#from uicheckapp.services import EchoService
+
+counterP = Counter('my_failures', 'Description of counter')
+start_http_server(8001)
+
+LOG_LEVEL: str = "DEBUG"
+FORMAT: str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging_config = {
+    "version": 1, # mandatory field
+    # if you want to overwrite existing loggers' configs
+    # "disable_existing_loggers": False,
+    "formatters": {
+        "basic": {
+            "format": FORMAT,
+        }
+    },
+    "handlers": {
+        "console": {
+            "formatter": "basic",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stderr",
+            "level": LOG_LEVEL,
+        }
+    },
+    "loggers": {
+        "simple_example": {
+            "handlers": ["console"],
+            "level": LOG_LEVEL,
+            # "propagate": False
+        }
+    },
+}
+
+# create logger
+logger = logging.getLogger('simple_example')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warning('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
 
 class Item(BaseModel):
     name: str
@@ -10,7 +69,7 @@ class Item(BaseModel):
     is_offer: Union[bool, None] = None
 
 app = FastAPI()
-
+log = logging.getLogger("simple_example")
 
 app.get("/")
 def read_root():
@@ -18,6 +77,14 @@ def read_root():
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json() }
 
+@app.get("/log_now")
+def log_now():
+    log.debug("/api/log_now starts")
+    log.info("I'm logging")
+    log.warning("some warnings")
+    counterP.inc()     # Increment by 1
+    print(counterP)
+    return {"result": "OK", "Counter": str(counterP)}
 
 @app.get("/items")
 def read_root():
@@ -40,11 +107,11 @@ def update_item(item_id: int, item: Item):
 def delete_item(item_id: int, item: Item):
     url = 'https://630287099eb72a839d7105f1.mockapi.io/items'
     delete_item = requests.delete(url, json = item.json(), timeout =5)
-    return Item.text()
+    return Item.text
 
 @app.post("/items/")
 def save_item(item: Item):
     url = 'https://630287099eb72a839d7105f1.mockapi.io/items'
-    new_item = requests.post(url, json = item.json(), timeout =5)
+    new_item = requests.post(url, data=item.json(), timeout =5,  headers={'Content-Type': 'application/json'})
     print(item.json())
-    return new_item.text()
+    return new_item.text
